8b2c7f3273899863737bc75d6cc9784ca37c1c10
Fernando Guillen <fguillen.mail@gmail.com>
2011-11-16 00:20:07 +0100

    BackBone version start working

--- client/assets/application2.js
+++ client/assets/application2.js
@@ -3,7 +3,7 @@ $(function(){
     initialize: function( options ){
       this.address = options.address;
     },
-    
+
     broadcast: function( message ){
       console.log( "broadcast: ", message );
       this.socket.send( JSON.stringify( message ) );
@@ -11,10 +11,10 @@ $(function(){
 
     connect: function(){
       console.log( "connecting to: ", this.address );
-      
-      _self = this;
+
+      var _self = this;
       this.trigger( "connecting" );
-      
+
       var Socket = ("MozWebSocket" in window) ? MozWebSocket : WebSocket;
       this.socket = new Socket("ws://" + this.address + ":8080/");
 
@@ -40,127 +40,158 @@ $(function(){
         _self.trigger( "connected" );
       };
     }
-    
+
   });
 
   var AppView = Backbone.View.extend({
     defaults: {
       // "address": "127.0.0.1",
       "address": "animalchat.fernandoguillen.info"
-      
+
     },
-    
+
     el: "body",
-    
+
 
     initialize: function( options ){
       console.log( "initialize AppView" );
       console.log( "AppView.address: ", options.address );
-      
+
       this.address  = options.address;
-      
-      var meId    = Math.floor(Math.random() * 1000) + "" + (new Date()).getTime();
-      var meColor = AppView.colors[ (meId - 1) % AppView.colors.length ]
-      
-      this.me       = new Actor({ id: meId , color: meColor });
+
       this.actors   = new Actors();
-      this.scene    = new MessagesView();
       this.alert    = new Alert();
-      
+      this.me       = null;
+
       console.log( "alert.status: ", this.alert.get('status') );
-      
+
       this.radio    = new Radio({ address: this.address });
-      
+
       this.alertView = new AlertView({ model: this.alert });
       this.alertView.render();
+
+      this.actorsView = new ActorsView({ collection: this.actors });
       
+      this.keys = new Keys();
+      this.keysView = new KeysView({ collection: this.keys });
+
       this.radio.bind( "connectionError" , this.alert.error      , this.alert );
       this.radio.bind( "connecting"      , this.alert.connecting , this.alert );
-      this.radio.bind( "connected"       , this.sayHello         , this );
+      this.radio.bind( "connected"       , this.alert.connected  , this.alert );
       this.radio.bind( "messageReceived" , this.messageReceived  , this );
-      
+
       this.radio.connect();
-      
-      this.actors.bind( 'add', this.addActor, this );
-      this.actors.add( this.me );
+
+
+
+      var _self = this;
+      $("#retry-connection").click( function(){
+        _self.radio.connect();
+      });
     },
-    
+
     messageReceived: function( data ){
+      var data = jQuery.parseJSON( data );
       console.log( "messageReceived: ", data );
-    },
-    
-    sayHello: function(){
-      console.log( "sayHello" );
-      
-      this.radio.broadcast({
-        type: "hello",
-        actor: this.me.toJSON()
-      })
-    },
-    
-    addActor: function( actor ){
-      console.log( "addActor: ", actor );
-      var view = new ActorView({ model: actor });
-      $("#actors ul").append( view.render() );
+      console.log( "messageReceived type: ", data.type );
+
+      switch( data.type ){
+        case "hello":
+          console.log( "message hello received" );
+          this.me = new Actor( data.actor );
+          this.actors.add( this.me );
+
+          break;
+          
+        case "actor":
+          console.log( "message actor received" );
+          var actor = new Actor( data.actor );
+          this.actors.add( actor );
+
+          break;
+
+        case "actors":
+          console.log( "message actors received" );
+          var _self = this;
+
+          this.actors.reset( data.actors );
+          // for( index in data.actors ){
+          //   actor_json = data.actors[ index ];
+          //   console.log( "adding actor_json: ", actor_json );
+          //   actor = new Actor( actor_json );
+          //   console.log( "adding actor: ", actor );
+          //   _self.actors.add( actor );
+          // }
+
+          break;
+          
+        case "goodbye":
+          console.log( "message goodbye received" );
+          var actor = new Actor( data.actor );
+          this.actors.remove( actor );
+
+          break;
+
+        case "key":
+          console.log( "message key received" );
+          var key = new Key({ color: data.actor.color, key: data.key });
+          this.keys.add( key );
+          
+          break;
+        default:
+          console.error( "message type unknown: " + data.type );
+      }
     },
 
     events: {
       "keypress": "sendKey"
     },
-    
+
     sendKey: function( event ){
       console.log( "key pressed: ", event );
-      this.radio.broadcast( event.which );
+      this.radio.broadcast( { type: "key", actor: this.me, key: String.fromCharCode( event.which ) } );
     }
 
 
-  }, {
-    colors: [
-      "#00FF00",
-      "#00FFFF",
-      "#FFFF00",
-      "#006600",
-      "#0066FF",
-      "#003300",
-      "#0033FF",
-      "#000000",
-      "#FF6600",
-      "#FF66FF",
-      "#FF3300"
-    ], 
   });
 
 
   var Alert = Backbone.Model.extend({
     initialize: function( options ){
     },
-    
+
     defaults: {
       "status": "hidden",
     },
-    
+
     error: function(){
       console.log( "alert to error" );
       console.log( "this.get('status'): ", this.get('status') );
       this.set({ status: "error" });
       console.log( "this.get('status'): ", this.get('status') );
     },
-    
+
     connecting: function(){
       console.log( "alert to connecting" );
       this.set({ status: "connecting" });
     },
-    
+
+    connected: function(){
+      console.log( "alert to connected" );
+      this.set({ status: "connected" });
+      this.set({ status: "hidden" });
+    },
+
     hidden: function(){
       console.log( "alert to hidden" );
       this.set({ status: "hidden" });
     },
-    
+
     initialize: function(){
       console.log( "Alert initialize" );
       console.log( "Alert status: " + this.get('status') );
 
-      
+
     }
   });
 
@@ -171,18 +202,34 @@ $(function(){
       this.model.bind( "change", this.render, this );
       console.log( "initalizing AlertView, model.status: ", this.model.get( 'status' ) );
     },
-    
+
     render: function(){
       console.log( "rendering alert: ", this.model.get( 'status' ) );
       switch( this.model.get( 'status' ) ){
         case "hidden":
           console.log( "alert status hidden" );
+
+          $('#light-box #connecting-problem').stop(true).fadeOut('fast');
+          $('#light-box #connecting').stop(true).fadeOut('fast');
+          $('#light-box').stop(true).fadeOut('fast');
+
           break;
         case "error":
           console.log( "alert status error" );
+
+          $('#light-box').stop(true).show();
+          $('#light-box #connecting').stop(true).fadeOut();
+          $('#light-box #connecting-problem').stop(true).fadeIn('slow');
+
           break;
         case "connecting":
           console.log( "alert status connecting" );
+
+          $('#light-box #connecting-problem').stop(true).hide();
+          $('#light-box').stop(true).show();
+          $('#light-box #connecting').stop(true).show();
+
+
           break;
         default:
           console.error( "alert status not found: ", this.model.get( 'status' ) );
@@ -195,17 +242,63 @@ $(function(){
     },
 
     initialize: function( data ){
+      console.log( "initialize actor: ", data );
       this.id     = data.id;
       this.color  = data.color;
     }
   });
 
   var Actors = Backbone.Collection.extend({
-    model: Actor
+    model: Actor,
+
+    initialize: function(){
+    },
+
+  });
+
+  var ActorsView = Backbone.View.extend({
+    el: "#actors ul",
+
+    initialize: function( options ){
+      console.log( "ActorsView initialize" );
+
+      this.collection.bind( 'add',      this.addActor,    this );
+      this.collection.bind( 'reset',    this.render,      this );
+      this.collection.bind( 'remove',   this.removeActor, this );
+    },
+
+    addActor: function( actor ){
+      console.log( "addActor: ", actor );
+      var view = new ActorView({ model: actor });
+      $(this.el).append( view.render() );
+    },
+    
+    removeActor: function( actor ){
+      console.log( "removeActor: ", actor );
+      $(this.el).find("li#actor-" + actor.id ).fadeOut('slow');
+    },
+
+    render: function(){
+      console.log( "ActorsView render" );
+
+      var _self = this;
+      this.collection.each( function( actor ){
+        console.log( "creating new ActorView: ", actor );
+        var view = new ActorView({ model: actor });
+        console.log( "this: ", _self );  
+        console.log( "el: ", $(_self.el) );
+        $(_self.el).append( view.render() );
+      });
+    }
+
+
+
+
   });
 
   var ActorView = Backbone.View.extend({
     template: _.template( "<li id='actor-<%= id %>' style='background-color: <%= color %>'>xx</li>" ),
+
     bump:     function(){},
     unbump:   function(){},
     render:   function(){
@@ -214,24 +307,44 @@ $(function(){
     }
   });
 
-  var ActorsView = Backbone.View.extend({
-    el:           "#actors ul",
-    collection:   Actors
-  });
-
-  var Message = Backbone.Model.extend({
-    initialize: function( message ){
-      console.log( "creating message: ", message );
+  var Key = Backbone.Model.extend({
+    initialize: function( options ){
+      console.log( "creating key: ", options );
+      this.color = options.color;
+      this.key   = options.key;
     }
   });
 
-  var Messages = Backbone.Collection.extend({
-    model: Message
+  var Keys = Backbone.Collection.extend({
+    initialize: function( options ){
+    }
   });
 
-  var MessagesView = Backbone.View.extend({
-    el: "scene",
-    collection: Messages
+  var KeyView = Backbone.View.extend({
+    template: _.template( "<span style='color: <%= color %>'><%= key %></span>" ),
+    render:   function(){
+      console.log( "rendering KeyView: ", this.model.toJSON() );
+      return this.template( this.model.toJSON() );
+    }
+  });
+
+  var KeysView = Backbone.View.extend({
+    el: "#scene",
+    
+    initialize: function(){
+      console.log( "KeysView initialize" );
+      
+      this.collection.bind( "add", this.addKey, this );
+    },
+    
+    addKey: function( key ){
+      console.log( "addKey: ", key );
+      console.log( "addKey escaped: ", escape( key.key ) );
+      key.key = escape( key.key );
+      console.log( "addKey: ", key );
+      var view = new KeyView({ model: key });
+      $(this.el).append( view.render() );
+    }
   });
 
 
--- server/server.rb
+++ server/server.rb
@@ -1,14 +1,49 @@
+require 'ostruct'
 require 'em-websocket'
 require 'json'
 
 
+class Actor
+  attr_accessor :id, :color, :sid, :socket
+  
+  def to_json
+    {
+      :id     => @id,
+      :color  => @color,
+    }
+  end
+end
+
+@colors = [
+  "#00FF00",
+  "#00FFFF",
+  "#FFFF00",
+  "#006600",
+  "#0066FF",
+  "#003300",
+  "#0033FF",
+  "#000000",
+  "#FF6600",
+  "#FF66FF",
+  "#FF3300"
+]
+
+def get_color( index )
+  @colors[ (index - 1) % @colors.size ]
+end
+
+def create_id
+  "#{rand(1000)}#{Time.now.to_i}".to_i
+end
+
+@sockets    = []
 @actors     = []
 @channel    = EM::Channel.new
 @last_index = 0
 
 def send_actors( actor )
-  actor[:ws].send(
-    {:type => "actors", :actors => @actors.map{ |e| e[:id] }}.to_json
+  actor.socket.send(
+    {:type => "actors", :actors => @actors.map{ |e| e.to_json }}.to_json
   )
 end
 
@@ -16,42 +51,61 @@ def next_index
   @last_index += 1
 end
 
+
 EventMachine.run {
 
-  EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080, :debug => true) do |ws|
-    
-    actor = {}
-    
-    ws.onopen {
-      actor = {:id => next_index, :ws => ws}
+  EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080, :debug => true) do |socket|
 
-      ws.send( {:type => "hello", :id => actor[:id]}.to_json )
+    actor = Actor.new
+    actor.socket  = socket
+    actor.id      = create_id
+    actor.color   = get_color( actor.id )
+
+    socket.onopen {
+      # actor = {:socket => socket}
+
+
+      # socket.send( {:type => "hello", :id => actor[:id]}.to_json )
+
+      # @actors << actor
+
+      # @channel.push({:type => "actor", :id => actor[:id]}.to_json)
+      
+      actor.socket.send({ :type => "hello", :actor => actor.to_json}.to_json )
+
+      send_actors( actor )
       
       @actors << actor
       
-      @channel.push(
-        {:type => "actor", :id => actor[:id]}.to_json
-      )
+      @channel.push( {:type => "actor", :actor => actor.to_json}.to_json )
       
-      actor[:sid] = @channel.subscribe { |msg| actor[:ws].send( msg ) }
+      sid = @channel.subscribe { |msg|
+        puts "XXX: msg: #{msg.inspect}"
+        actor.socket.send( msg )
+      }
       
-      send_actors( actor )
+      actor.sid = sid
+      
+      
+
     }
 
-    ws.onmessage { |msg|
-      @channel.push(
-        {:type => "message", :id => actor[:id], :char => msg.gsub(/[^\w\s]/, '-')}.to_json
-      )
-    }
+    socket.onmessage do |msg|
+      puts "XXX: msg: #{msg}"
+      msg = JSON.parse( msg )
+      puts "XXX: msg JSON: #{msg}"
+      
+      @channel.push( msg.to_json )
+    end
 
-    ws.onclose {
-      @channel.unsubscribe( actor[:sid] )
-      @channel.push( 
-        {:type => "goodbye", :id => actor[:id]}.to_json
+    socket.onclose {
+      puts "XXX: actor closing: #{actor.id}"
+      @channel.unsubscribe( actor.sid )
+      @channel.push(
+        {:type => "goodbye", :actor => actor.to_json}.to_json
       )
       @actors.delete( actor )
     }
-
   end
 
   puts "Server started"
